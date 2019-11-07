76e202c46a5b415d266c192b06b795d03404ba37
Doug <deguirlinger@gmail.com>
2019-11-07 09:21:30 -0800

    added cqrs handler and seeded more data

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Persistence
         }
 
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
