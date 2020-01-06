a6783adbcdcdcd765918de0ddebf1954a1045c34
Doug <deguirlinger@gmail.com>
2020-01-06 11:12:22 -0800

    added registration form

--- client-app/src/features/user/RegisterForm.tsx
+++ client-app/src/features/user/RegisterForm.tsx
@@ -9,17 +9,19 @@ import { combineValidators, isRequired } from 'revalidate';
 import { ErrorMessage } from '../../app/common/form/ErrorMessage';
 
 const validate = combineValidators({
+  username: isRequired('username'),
+  displayName: isRequired('display name'),
   email: isRequired('email'),
   password: isRequired('password')
 });
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const rootStore = useContext(RootStoreContext);
-  const { login } = rootStore.userStore;
+  const { register } = rootStore.userStore;
   return (
     <FinalForm
       onSubmit={(values: IUserFormValues) =>
-        login(values).catch(error => ({
+        register(values).catch(error => ({
           [FORM_ERROR]: error
         }))
       }
@@ -35,9 +37,15 @@ export const LoginForm = () => {
         <Form onSubmit={handleSubmit} error>
           <Header
             as="h2"
-            content="Login to Reactivities"
+            content="Sign up for Reactivities"
             color="teal"
             textAlign="center"
+          />
+          <Field name="username" component={TextInput} placeholder="Username" />
+          <Field
+            name="displayName"
+            component={TextInput}
+            placeholder="Display Name"
           />
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
@@ -47,16 +55,13 @@ export const LoginForm = () => {
             type="password"
           />
           {submitError && !dirtySinceLastSubmit && (
-            <ErrorMessage
-              error={submitError}
-              text="Invalid email or password"
-            />
+            <ErrorMessage error={submitError} />
           )}
           <Button
             disabled={(invalid && !dirtySinceLastSubmit) || pristine}
             loading={submitting}
             color="teal"
-            content="Login"
+            content="Register"
             fluid
           />
         </Form>
