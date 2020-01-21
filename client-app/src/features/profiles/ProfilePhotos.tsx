ec21545c0701568c120628c4040b281ce9f238ee
Doug <deguirlinger@gmail.com>
2020-01-20 16:55:20 -0800

    added setMain and delete photo to client app

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -10,9 +10,16 @@ const ProfilePhotos = () => {
     profile,
     isCurrentUser,
     uploadPhoto,
-    uploadingPhoto
+    uploadingPhoto,
+    setMainPhoto,
+    deletePhoto,
+    loading
   } = rootStore.profileStore;
-  const [addPhotoMode, setAddPhotoMode] = useState(true);
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [deleteTarget, setDeleteTarget] = useState<string | undefined>(
+    undefined
+  );
 
   const handleUploadImage = (photo: Blob) => {
     uploadPhoto(photo).then(() => setAddPhotoMode(false));
@@ -47,8 +54,30 @@ const ProfilePhotos = () => {
                       <Image src={photo.url} />
                       {isCurrentUser && (
                         <Button.Group fluid widths={2}>
-                          <Button basic positive content="Main" />
-                          <Button basic negative icon="trash" />
+                          <Button
+                            name={photo.id}
+                            onClick={e => {
+                              setMainPhoto(photo);
+                              setTarget(e.currentTarget.name);
+                            }}
+                            disabled={photo.isMain}
+                            loading={loading && target === photo.id}
+                            basic
+                            positive
+                            content="Main"
+                          />
+                          <Button
+                            name={photo.id}
+                            disabled={photo.isMain}
+                            onClick={e => {
+                              deletePhoto(photo);
+                              setDeleteTarget(e.currentTarget.name);
+                            }}
+                            loading={loading && deleteTarget === photo.id}
+                            basic
+                            negative
+                            icon="trash"
+                          />
                         </Button.Group>
                       )}
                     </Card>
