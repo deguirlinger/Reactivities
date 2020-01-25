9b6552715f2b0ffb7c5ed6fd8c905b16a1d81a70
Doug <deguirlinger@gmail.com>
2020-01-25 10:58:07 -0800

    added comment entity

---
+++ Persistence/Migrations/20200125183310_AddedComentEntity.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddedComentEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Body = table.Column<string>(nullable: true),
+                    AuthorId = table.Column<string>(nullable: true),
+                    ActivityId = table.Column<Guid>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 1, 26, 10, 33, 10, 117, DateTimeKind.Local).AddTicks(5630));
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 1, 27, 10, 33, 10, 135, DateTimeKind.Local).AddTicks(2710));
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 1, 28, 10, 33, 10, 135, DateTimeKind.Local).AddTicks(2760));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ActivityId",
+                table: "Comments",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId",
+                table: "Comments",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 1, 17, 9, 57, 21, 314, DateTimeKind.Local).AddTicks(2090));
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 1, 18, 9, 57, 21, 332, DateTimeKind.Local).AddTicks(2710));
+
+            migrationBuilder.UpdateData(
+                table: "WeatherForecasts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 1, 19, 9, 57, 21, 332, DateTimeKind.Local).AddTicks(2750));
+        }
+    }
+}
